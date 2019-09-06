2c4d554b155cf61fb0378f98e984b2c5e07000c1
Dan <danandreidobos@gmail.com>
2019-09-06 14:43:16 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,140 @@
+/* Open when someone clicks on the span element */
+function openNav1() {
+  document.getElementById("TheSquare").style.width = "100%";
+}
+function openNav2() {
+  document.getElementById("AltSquare").style.width = "100%";
+}
+function openNav3() {
+  document.getElementById("Glod").style.width = "100%";
+}
+function openNav4() {
+  document.getElementById("Contact").style.width = "100%";
+}
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav1() {
+  document.getElementById("TheSquare").style.width = "0%";
+}
+function closeNav2() {
+  document.getElementById("AltSquare").style.width = "0%";
+}
+function closeNav3() {
+  document.getElementById("Glod").style.width = "0%";
+}
+function closeNav4() {
+  document.getElementById("Contact").style.width = "0%";
+}
+/*Particles*/
+particlesJS('particles-js',
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "polygon",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 4
+        },
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 10,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+);
